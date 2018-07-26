9bcef780bc1ebb4f956586e47e0fee681f523fa7
senthilrajd <senthilrajdhanushkodi@gmail.com>
2018-07-26 17:46:56 +0530

    Updated binary search code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,43 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int a[] = new int[]{1,2,3,4,5};
+
+        int length = a.length;
+        int start = 0;
+        int find_number = 5;
+
+        int pos = BinarySearch(a, start, length-1, find_number);
+        System.out.println(pos);
     }
+
+
+    private static int BinarySearch(int a[], int start, int length, int find_number)
+    {
+
+        if(length == start)
+        {
+            if(a[length] == find_number)
+                return length;
+        }
+        if(length > start) {
+            int mid = start + (length - 1 ) / 2;
+
+            if (a[mid] == find_number) {
+                return mid;
+            }
+
+            if (a[mid] > find_number) {
+                return BinarySearch(a, start, mid - 1, find_number);
+            }
+
+            return BinarySearch(a, mid + 1, length, find_number);
+        }
+        else
+            return -1;
+
+    }
+
+
 }
